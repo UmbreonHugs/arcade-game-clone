48caa2f349f57c428f78e48561011ea9bcafc50b
Umbreon (Luigi R.) <thegaminggodlou@gmail.com>
2018-08-26 15:14:30 -0700

    Added difficulty, death, and fixed collision

--- js/app.js
+++ js/app.js
@@ -12,6 +12,12 @@ var Enemy = function(x, y, speed) {
     this.h = 101;
     this.w = 171;
 };
+// easy = 1
+// medium = 2
+// hard = 3
+// expert = 4
+let difficulty = 0;
+let death = 0;
 // characters
 const char = {
     boy: "images/char-boy.png",
@@ -37,13 +43,27 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x + 80 > player.x && this.x < player.x + 80 && this.y == player.y) {
-    // collision detected!
-      console.log("collision detected");
-    } else if (this.x > 500) {
-      this.x == 0; //reset position
-    }
+    this.dt = dt;
     this.x = this.x + this.speed * dt;
+
+    if (this.x > 550) {
+      // randomize the speed after each turn
+      this.speed = 100 + Math.floor(Math.random() * 520);
+      this.x = -100;
+    }
+    // collision detection, see https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    if (this.x + 60 > player.x && this.x < player.x + 60 && player.y < this.y + 25 && 30 + player.y > this.y) {
+    // collision detected! set player position, add to death
+      player.x = 200;
+      player.y = 400;
+      death += 1;
+    }
+    if (player.y < 0) {
+          this.speed += 80 + Math.floor(Math.random() * 520);
+    }
+    /* else if (this.x > 500) {
+      this.x = 0; //reset position
+    } */
 };
 
 // Draw the enemy on the screen, required method for game
@@ -56,9 +76,11 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 Player.prototype.update = function() {
   if (this.y < 0) {
-    // return to position, add score
+    // return to position, add score, increase speed
     this.y = 400;
   }
+  // keep the player in bounderies
+
 };
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -88,10 +110,17 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy(0, 220, 70);
-var enemy2 = new Enemy(0, 140, 60);
-var enemy3 = new Enemy(0, 60, 50);
-const allEnemies = [enemy1, enemy2, enemy3];
+
+
+// expert level
+var enemy1 = new Enemy(0, 220, 320);
+var enemy2 = new Enemy(0, 140, 90);
+var enemy3 = new Enemy(0, 60, 180);
+var enemy4 = new Enemy(100, 60, 140);
+var enemy5 = new Enemy(200, 220, 230);
+var enemy6 = new Enemy(300, 60, 250);
+var enemy7 = new Enemy(200, 60, 280);
+const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7];
 let player = new Player(200, 400, 50);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
